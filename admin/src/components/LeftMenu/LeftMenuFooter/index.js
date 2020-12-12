cca5ee88b4289277fbc23301afec5b818d250b74
henrique oliveira <henrique_trind@hotmail.com>
2020-12-12 13:19:09 -0300

    personalize left menu of strapi admin panel

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+import Wrapper from './Wrapper';
+
+function LeftMenuFooter() {
+  // PROJECT_TYPE is an env variable defined in the webpack config
+  // eslint-disable-next-line no-undef
+  const projectType = PROJECT_TYPE;
+
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        Powered by &nbsp;
+        <a key="website" href="https://reactavancado.com.br" target="_blank" rel="noopener noreferrer">
+          React Avançado
+        </a>
+      </div>
+    </Wrapper>
+  );
+}
+
+
+export default LeftMenuFooter;
