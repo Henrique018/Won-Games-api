ca3893619001f81755b721f1bd1f2d7f04ed3a25
henrique oliveira <henrique_trind@hotmail.com>
2021-06-26 16:35:32 -0300

    turn amountLimit & tracing on

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //
+  graphql: {
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 5000,
+    apolloServer: {
+      tracing: true,
+    },
+  },
+};
